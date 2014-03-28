12f29d2afa127046401d67d505c3c53294c2fc26
Craig Robson <c.a.robson1@ncl.ac.uk>
2014-03-28 15:31:34 +0000

    Updated so builds networks adding a speed and thus time to travel an edge based on th attirbutes in the network shapefile. Untested to see if still works with a network with no weights.

--- ncl_network_sim/__init__.py
+++ ncl_network_sim/__init__.py
@@ -10,9 +10,7 @@ def build_network(shp_file):
     shapes = sf.shapes()
 
     #create graph
-    orig_G=nx.Graph()
-    graph_edges = []
-    
+    orig_G=nx.Graph()  
     
     out_nodes = []
     out_nodes_geom_lookup_dict = {} 
@@ -27,13 +25,22 @@ def build_network(shp_file):
     bottom = []
     right = []
     top = []
+   
+    #calculates the field the speed attribute is in
+    speed_field = 0
+    while speed_field < len(sf.fields):
+        if sf.fields[speed_field][0] == 'speed': 
+            speed_field -= 1 #as the first field is not an actual attribute!
+            break
+        speed_field += 1 #if no speed field, will value will be greater than the number of fields so will get an error
+    
+    edge_count = 0
     for shape in shapes:
         l,b,r,t = shape.bbox
         left.append(l)
         bottom.append(b)
         right.append(r)
         top.append(t)
-        metro_line_points = []
         
         stat1 = shape.points[0]
         stat2 = shape.points[-1]
@@ -53,17 +60,30 @@ def build_network(shp_file):
             #the correct geometry is then stored in a dictionary so that i can go from simplfied geom to real world geom
             p1 = all_nodes_geom_lookup_dict.get(_truncate_geom(shape.points[i]),Node(shape.points[i],_truncate_geom(shape.points[i])))
             p2 = all_nodes_geom_lookup_dict.get(_truncate_geom(shape.points[i+1]),Node(shape.points[i+1],_truncate_geom(shape.points[i+1])))
+
             for p in [p1,p2]:
                 all_nodes_geom_lookup_dict[p._truncated_geom] = p
             edge_class = Edge(p1,p2)
-            if (p1._truncated_geom,p2._truncated_geom) not in graph_edges:
-                graph_edges.append((p1._truncated_geom,p2._truncated_geom))
+            
+            if (p1._truncated_geom,p2._truncated_geom) not in orig_G.edges():
+                #add the edge straight to the network on the discovery that it has not been added as yet
+                #get the speed for the edge 
+                speed = sf.record(edge_count)[speed_field]
+                #calculate the length of the edge
+                orig_e,orig_n = p1._truncated_geom
+                dest_e,dest_n = p2._truncated_geom
+                #I know this calculation can be done mch easier, but can't remember how!
+                temp1 = (orig_e-dest_e)
+                temp2 = (orig_n-dest_n)
+                length = math.sqrt((temp1*temp1)+(temp2*temp2))
+                #calculate the time to travel the edge
+                time = length/speed
+                #add the edge and its attributes
+                orig_G.add_edge(p1._truncated_geom,p2._truncated_geom,{'length':length,'speed':speed,'time':time})
                 edge_geom_lookup_dict[(p1._truncated_geom,p2._truncated_geom)] = edge_class
                 edges.append(edge_class)
-                
-    #adds all simple edges to graph
-
-    orig_G.add_edges_from(graph_edges)
+        #iterate the edge count - used to get speeds assigned to the edges
+        edge_count += 1 
         
     _bbox = (min(left),min(bottom),max(right),max(top))
     
